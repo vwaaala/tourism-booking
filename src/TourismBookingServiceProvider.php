d0bf269a17df952943c7b19b7f418486865d340d
Rafiqul Islam Shawn <vwaaala@gmail.com>
2024-02-21 17:45:34 +0600

    first commit

---
+++ src/TourismBookingServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Bunker\TourismBooking;
+
+use Closure;
+use Illuminate\Support\ServiceProvider;
+
+class TourismBookingServiceProvider extends ServiceProvider
+{
+    public function boot(): void
+    {
+
+    }
+
+    public function register()
+    {
+
+    }
+}
